9b972d890d7802fada9142ad7d1496b49c6005fe
Nobel Okelekele <34259322+lordBman@users.noreply.github.com>
2023-07-13 11:46:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value} Calabar Poeple</div>;
+  return <div>Hello {value} Calabar Poeple, how do you do</div>;
 }
 
 export default App;
